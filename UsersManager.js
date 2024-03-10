7ae8c0b08ac80f14053d5d29907a21f4a72f0c1c
OscarBarreto09 <134447307+OscarBarreto09@users.noreply.github.com>
2024-03-10 16:26:22 -0500

    usuarios (#1)

---
+++ UsersManager.js
@@ -0,0 +1,43 @@
+class UserManager {
+  static #users = [];
+  create(data) {
+    const user = {
+      id:
+        UserManager.#users.length === 0
+          ? 1
+          : UserManager.#users[UserManager.#users.length - 1].id + 1,
+      photo: data.photo,
+      email: data.email,
+      password: data.password,
+      role: 0,
+    };
+    UserManager.#users.push(user);
+    console.log("nuevousuario");
+  }
+  read(){
+    return UserManager.#users
+  }
+}
+
+const usuarios = new UserManager()
+usuarios.create({
+    photo: "photo1.png",
+    email: "usuario1@gmail.com",
+    password: 1234
+})
+
+usuarios.create({
+    photo: "photo2.png",
+    email: "usuario2@gmail.com",
+    password: 5678
+})
+
+usuarios.create({
+    photo: "photo3.png",
+    email: "usuario3@gmail.com",
+    password: 9874
+})
+
+console.log(usuarios.read())
+
+//
